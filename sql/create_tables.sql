8811fa0d126b092906c6bd03517056c0efa56fde
Tom Modeste <tom.modeste@gmail.com>
2023-08-27 14:47:11 +0200

    nfr: added users to seed

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -19,39 +19,6 @@ CREATE TABLE IF NOT EXISTS user_account (
   PRIMARY KEY (u_id)
 );
 
--- Creation of post table
-CREATE TABLE IF NOT EXISTS post (
-  p_id        uuid NOT NULL DEFAULT gen_random_uuid(), -- NOT NULL DEFAULT ?
-	title       VARCHAR(60),
-	details     TEXT,
-	owner_id    uuid,
-  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  PRIMARY KEY (p_id),
-  CONSTRAINT  fk_owner FOREIGN KEY(owner_id) REFERENCES user_account(u_id)
-);
-
--- Creation of comment table
-CREATE TABLE IF NOT EXISTS comment (
-  c_id        uuid NOT NULL DEFAULT gen_random_uuid(),
-	content     VARCHAR(320),
-	owner_id    uuid,
-	parent_id   uuid,
-  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  PRIMARY KEY (c_id),
-  CONSTRAINT  fk_owner FOREIGN KEY(owner_id) REFERENCES user_account(u_id),
-  CONSTRAINT  fk_parent FOREIGN KEY(parent_id) REFERENCES post(p_id)
-);
-
 CREATE TRIGGER update_customer
 	BEFORE UPDATE ON user_account
-	FOR EACH ROW EXECUTE PROCEDURE updated_at();
-
-CREATE TRIGGER update_post
-	BEFORE UPDATE ON post
-	FOR EACH ROW EXECUTE PROCEDURE updated_at();
-
-CREATE TRIGGER update_comment
-	BEFORE UPDATE ON comment
 	FOR EACH ROW EXECUTE PROCEDURE updated_at();
---
+++ sql/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO user_account (u_id, u_username, u_password, email, created_at)
+VALUES
+	('3afdb6e8-f8a3-461a-a3fb-0dca1ff6bcfb', 'Tom Modeste', 'tata', 't.m@g.com', 'Sun Aug 27 2023 10:36:12'),
+	('edf63d7e-2cf8-46b3-8fc2-09346767127d', 'Gordon Freeman', 'tete', 'g.freeman@black-mesa.edu', 'Sun Aug 27 2023 10:36:22'),
+	('04a43efa-179c-49b4-a819-8915f0eabaf5', 'Isaac Kleiner', 'titi', 'i.kleiner@black-mesa.edu', 'Sun Aug 27 2023 10:36:32'),
+	('18dbbe23-e7ca-4ac1-bcea-46bd7dbf89a9', 'Gina Cross', 'toto', 'g.cross@black-mesa.edu', 'Sun Aug 27 2023 10:36:42'),
+	('211b9dfd-f0e4-45ed-b697-27267dd5e6fd', 'Colette Green', 'tutu', 'c.green@black-mesa.edu', 'Sun Aug 27 2023 10:36:52'),
+	('0a6c5da3-71bf-4278-942f-983c039d00bd', 'Alyx Vance', 'tyty', 'a.vance@black-mesa.edu', 'Sun Aug 27 2023 10:37:02'),
+	('10219528-6c2f-4caa-a742-8715ae6041d5', 'Eli Vance', 'toutou', 'e.vance@black-mesa.edu', 'Sun Aug 27 2023 10:37:12'),
+	('8b9faffb-bb70-4359-983c-3ede76868aa2', 'Barney Calhoun', 'tonton', 'b.calhoun@black-mesa.edu', 'Sun Aug 27 2023 10:37:22'),
+	('ff684f0c-3593-4568-9947-bcbaa7df6c69', 'Adrian Shepard', 'teuteu', 'cpl.shepard@marines.mil', 'Sun Aug 27 2023 10:37:32');
